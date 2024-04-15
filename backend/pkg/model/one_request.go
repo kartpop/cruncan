42e55c673624cf3185acec519d936b34425ec8cd
Kartikeya Pophali <kartikeyapophali@gmail.com>
2024-04-15 13:40:54 +0530

    chore: update one request to use slice of items

--- backend/pkg/model/one_request.go
+++ backend/pkg/model/one_request.go
@@ -3,11 +3,10 @@ package model
 type OneRequest struct {
 	UserID string `json:"user_id"`
 	Prompt string `json:"prompt"`
-	Data  Data `json:"data"`
-
+	Data   []Item `json:"data"`
 }
 
-type Data struct {
-	Key string `json:"key"`
+type Item struct {
+	Key   string `json:"key"`
 	Value string `json:"value"`
 }
