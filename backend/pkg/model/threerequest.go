579c21b304646902b5d8c25273b188b21b3b72db
Kartikeya Pophali <kartikeyapophali@gmail.com>
2024-05-22 20:29:12 +0530

    pkg: add id to threerequest

--- backend/pkg/model/threerequest.go
+++ backend/pkg/model/threerequest.go
@@ -2,6 +2,7 @@ package model
 
 // ThreeRequest is a model for the request to /three
 type ThreeRequest struct {
+	ID         string     `json:"id"`
 	Metadata   string     `json:"metadata"`
 	OneRequest OneRequest `json:"one_request"`
 }
